faf958711140768e70f32f8783b0858ba0f6d276
Kalle Elmér <kalle@silvertejp.nu>
2018-01-02 18:17:16 +0100

    Föreställningssidan visar antal (lediga) platser.

--- pages/performances/PerformanceCtrl.js
+++ pages/performances/PerformanceCtrl.js
@@ -14,9 +14,29 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 		Core.get("/admin/performances/" + $routeParams.id + "/seats").then(
 			function(response) {
 				$scope.performance.seats = response.data;
+				$scope.countStats();
 			}, function(response) {
 				alert("Kunde inte hämta platserna: " + response.status);
 			});
+	}
+
+	ctrl.changeStats = function(profile, key, change) {
+		if (!$scope.stats.hasOwnProperty(profile)) {
+			$scope.stats[profile] = {
+				"available" : 0,
+				"reserved" : 0
+			};
+		}
+		$scope.stats[profile][key] += change;
+	}
+
+	$scope.countStats = function() {
+		$scope.stats = {};
+		for (var i = 0; i < $scope.performance.seats.length; i++) {
+			var seat = $scope.performance.seats[i];
+			var key = seat.active_ticket_id == 0 ? "available" : "reserved";
+			ctrl.changeStats(seat.profile_name, key, 1);
+		}
 	}
 };
 
