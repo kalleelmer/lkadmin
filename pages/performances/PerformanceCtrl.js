857dc239cd00c88f4217b18c154f7ab8c84a7ab1
Kalle Elmér <kalle@silvertejp.nu>
2018-01-02 20:52:19 +0100

    Profiler och lediga platser hämtas från backend.

--- pages/performances/PerformanceCtrl.js
+++ pages/performances/PerformanceCtrl.js
@@ -7,36 +7,29 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 		$scope.performance = response.data;
 		ctrl.loadSeats();
 	}, function(response) {
-		alert("Kunde inte hämta nöjet: " + response.status);
+		alert("Kunde inte hämta föreställningen: " + response.status);
+	});
+
+	Core.get("/admin/performances/" + $routeParams.id + "/availability").then(
+		function(response) {
+			$scope.stats = response.data;
+		}, function(response) {
+			alert("Kunde inte hämta antal lediga platser: " + response.status);
+		});
+
+	Core.get("/admin/profiles").then(function(response) {
+		$scope.profiles = response.data;
+	}, function(response) {
+		alert("Kunde inte hämta profiler: " + response.status);
 	});
 
 	ctrl.loadSeats = function() {
 		Core.get("/admin/performances/" + $routeParams.id + "/seats").then(
 			function(response) {
 				$scope.performance.seats = response.data;
-				$scope.countStats();
 			}, function(response) {
 				alert("Kunde inte hämta platserna: " + response.status);
 			});
-	}
-
-	ctrl.changeStats = function(profile, key, change) {
-		if (!$scope.stats.hasOwnProperty(profile)) {
-			$scope.stats[profile] = {
-				"available" : 0,
-				"reserved" : 0
-			};
-		}
-		$scope.stats[profile][key] += change;
-	}
-
-	$scope.countStats = function() {
-		$scope.stats = {};
-		for (var i = 0; i < $scope.performance.seats.length; i++) {
-			var seat = $scope.performance.seats[i];
-			var key = seat.active_ticket_id == 0 ? "available" : "reserved";
-			ctrl.changeStats(seat.profile_name, key, 1);
-		}
 	}
 };
 
