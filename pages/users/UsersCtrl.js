bc8d8da671afa7c9d6027eac7337a72f3633a183
kristoffer <kristoffer.nordstrom@outlook.com>
2018-01-21 15:39:22 +0100

    Möjlighet att lägga till nya användare

--- pages/users/UsersCtrl.js
+++ pages/users/UsersCtrl.js
@@ -1,11 +1,22 @@
 var module = angular.module("lkticket.admin");
 
 var UsersCtrl = function(Core, $scope) {
+
 	Core.get("/admin/users").then(function(response) {
 		$scope.users = response.data;
 	}, function(response) {
 		alert("Kunde inte hämta användare: " + response.status);
 	});
+
+	$scope.createNewUser = function() {
+		console.log($scope.newuser);
+		Core.post("/admin/users", $scope.newuser).then(function(response) {
+			$scope.users.push(response.data);
+		}, function(response) {
+			alert(response.status);
+		});
+	}
+
 }
 
 module.controller("UsersCtrl", UsersCtrl);
