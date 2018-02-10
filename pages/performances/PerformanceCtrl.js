503a1587bd448a8ab17696522989077f07e56414
Kalle Elmér <kalle@silvertejp.nu>
2018-02-10 21:26:03 +0100

    Hantera kvoter per kategori.

--- pages/performances/PerformanceCtrl.js
+++ pages/performances/PerformanceCtrl.js
@@ -6,39 +6,37 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 	Core.get("/admin/performances/" + $routeParams.id).then(function(response) {
 		$scope.performance = response.data;
 		ctrl.loadSeats();
+		ctrl.loadAvailability();
 	}, function(response) {
 		alert("Kunde inte hämta föreställningen: " + response.status);
 	});
 
 	Core.get("/admin/profiles").then(function(response) {
 		$scope.profiles = response.data;
-		ctrl.loadAvailability();
 	}, function(response) {
 		alert("Kunde inte hämta profiler: " + response.status);
 	});
 
 	ctrl.loadAvailability = function() {
+		Core.get("/admin/shows/" + $scope.performance.show.id + "/categories").then(
+			function(response) {
+				$scope.categories = response.data;
+			},
+			function(response) {
+				alert("Kunde inte hämta platstyperna: " + response.status);
+			});
+
 		var req = Core.get("/admin/performances/" + $routeParams.id
 			+ "/availability");
 		req.then(function(response) {
-			$scope.noprofile = response.data["0"];
-			if(!$scope.noprofile) {
-				$scope.noprofile = {"total":0, "available":0};
-			}
-			for (var i = 0; i < $scope.profiles.length; i++) {
-				var p = $scope.profiles[i];
-				var stats = response.data[p.id];
-				if (stats) {
-					p.total = stats.total;
-					p.available = stats.available;
-				} else {
-					p.total = 0;
-					p.available = 0;
-				}
-			}
+			$scope.stats = response.data;
 		}, function(response) {
 			alert("Kunde inte hämta antal lediga platser: " + response.status);
 		});
+	}
+	
+	$scope.getStats = function(p, c) {
+		return $scope.stats[2][1];
 	}
 
 	ctrl.loadSeats = function() {
@@ -50,23 +48,39 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 			});
 	}
 
-	$scope.changeQuota = function(profile) {
+	$scope.changeQuota = function(profile, category) {
 		if(profile.working) {
 			return;
 		}
 		profile.working = true;
-		var numTickets = prompt("Antal biljetter", profile.total);
+		if(!$scope.stats[profile.id]) {
+			$scope.stats[profile.id] = {};
+		}
+		var stats = $scope.stats[profile.id][category.id];
+		if(!stats) {
+			stats = {available:0, total:0};
+			$scope.stats[profile.id][category.id] = stats;
+		}
+		if(!$scope.stats[0]) {
+			$scope.stats[0] = {};
+		}
+		if(!$scope.stats[0][category.id]) {
+			$scope.stats[0][category.id] = {available:0, total:0};
+		}
+	var numTickets = prompt("Antal biljetter", stats.total);
 		if(numTickets > 0 || numTickets === "0") {
-			var diff = numTickets - profile.total;
+			var diff = numTickets - stats.total;
 			if (diff > 0) {
-				ctrl.addTickets(profile, 0, diff);
+				ctrl.addTickets(profile, category, stats, 0, diff);
 			} else {
-				ctrl.removeTickets(profile, 0, -diff);
+				ctrl.removeTickets(profile, category, stats, 0, -diff);
 			}
+		} else {
+			profile.working = false;
 		}
 	}
 
-	ctrl.addTickets = function(profile, index, limit) {
+	ctrl.addTickets = function(profile, category, stats, index, limit) {
 		if (limit == 0) {
 			profile.working = false;
 			return;
@@ -76,26 +90,27 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 			return;
 		}
 		var seat = $scope.performance.seats[index];
-		if (seat.profile_id || seat.active_ticket_id) {
-			// Seat reserved or in another quota
-			ctrl.addTickets(profile, index + 1, limit);
+		if (seat.profile_id || seat.active_ticket_id || seat.category_id != category.id) {
+			// Seat reserved, in another quota, or wrong category
+			ctrl.addTickets(profile, category, stats, index + 1, limit);
 			return;
 		}
 		Core.put("/admin/seats/" + seat.id + "/profile", profile).then(
 			function(response) {
 				seat.profile_id = profile.id;
 				seat.profile_name = profile.name;
-				profile.total++;
-				profile.available++;
-				$scope.noprofile.total--;
-				$scope.noprofile.available--;
-				ctrl.addTickets(profile, index + 1, limit - 1);
+				stats.total++;
+				stats.available++;
+				$scope.stats[0][category.id].total--;
+				$scope.stats[0][category.id].available--;
+				ctrl.addTickets(profile, category, stats, index + 1, limit - 1);
 			}, function(response) {
 				alert("Tekniskt fel: " + response.status);
+				profile.working = false;
 			});
 	}
 
-	ctrl.removeTickets = function(profile, index, limit) {
+	ctrl.removeTickets = function(profile, category, stats, index, limit) {
 		if (limit == 0) {
 			profile.working = false;
 			return;
@@ -105,22 +120,23 @@ var PerformanceCtrl = function($filter, $scope, $http, User, $routeParams, Core)
 			return;
 		}
 		var seat = $scope.performance.seats[index];
-		if (seat.profile_id != profile.id || seat.active_ticket_id != 0) {
+		if (seat.profile_id != profile.id || seat.active_ticket_id != 0 || seat.category_id != category.id) {
 			// Seat reserved or not in this quota
-			ctrl.removeTickets(profile, index + 1, limit);
+			ctrl.removeTickets(profile, category, stats, index + 1, limit);
 			return;
 		}
 		Core.delete("/admin/seats/" + seat.id + "/profile").then(
 			function(response) {
 				seat.profile_id = 0;
 				seat.profile_name = null;
-				$scope.noprofile.total++;
-				$scope.noprofile.available++;
-				profile.total--;
-				profile.available--;
-				ctrl.removeTickets(profile, index + 1, limit - 1);
+				$scope.stats[0][category.id].total++;
+				$scope.stats[0][category.id].available++;
+				stats.total--;
+				stats.available--;
+				ctrl.removeTickets(profile, category, stats, index + 1, limit - 1);
 			}, function(response) {
 				alert("Tekniskt fel: " + response.status);
+				profile.working = false;
 			});
 	}
 };
